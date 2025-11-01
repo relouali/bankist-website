d9d7c49421c7525f9973012536d92842ff7dfec0
Rachid el Ouali <r.c.elouali@gmail.com>
2025-11-02 00:51:59 +0100

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -4,12 +4,15 @@ const section1 = document.querySelector('#section--1');
 
 ///////////////////////////////////////
 // Modal window
-
+// all tabs
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
+const nav = document.querySelector('.nav');
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -73,18 +76,10 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 const h1 = document.querySelector('h1');
 
-console.log(h1.querySelectorAll('.highlight'));
 h1.lastElementChild.style.color = 'white';
 h1.firstElementChild.style.color = 'black';
 
 // tabbed component
-
-// all tabs
-const tabs = document.querySelectorAll('.operations__tab');
-
-const tabsContainer = document.querySelector('.operations__tab-container');
-
-const tabsContent = document.querySelectorAll('.operations__content');
 
 // event handler for individual tabs
 tabsContainer.addEventListener('click', function (e) {
@@ -103,6 +98,22 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add(`operations__content--active`);
 });
 
+// Menu Fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('nav').querySelectorAll('.nav__link');
+    const logo = link.closest('nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 ////////////////////////////////////////////
 ////////////////////////////////////////////
 ////////////////////////////////////////////
